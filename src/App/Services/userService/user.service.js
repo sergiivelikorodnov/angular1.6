a6b44fb7abc81f8c54413a9dd9aee52e8c69462d
Sergii <inthe7heaven@gmail.com>
2024-11-30 15:51:23 +0100

    add: factory

--- src/App/Services/userService/user.service.js
+++ src/App/Services/userService/user.service.js
@@ -1,5 +1,21 @@
-angular.module('app').service('UserService', function ($http, USER_API_URL) {
+angular.module('app').service('UserService', function ($http) {
   this.getUsers = function () {
     return $http.get(USER_API_URL).then((response) => response.data);
+  };
+});
+
+angular.module('app').service('UserService', function ($http, USER_API_URL) {
+  const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  this.getUsers = function () {
+    return $http.get(USER_API_URL).then((response) => response.data);
+  };
+
+  this.getUserById = function (id) {
+    return $http.get(`${USER_API_URL}/${id}`).then((response) => response.data);
+  };
+
+  this.createUser = function (userData) {
+    return $http.post(USER_API_URL, userData).then((response) => response.data);
   };
 });
